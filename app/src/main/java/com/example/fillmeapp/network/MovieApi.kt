cd1e0c53a06635f6342045adbb4a39335ceb7f5d
Luis Vizcayno Barrera <luis.vizcayno.barrera@sap.com>
2024-05-17 17:19:11 +0200

    Serialization of the MovieDTO class

--- app/src/main/java/com/example/fillmeapp/network/MovieApi.kt
+++ app/src/main/java/com/example/fillmeapp/network/MovieApi.kt
@@ -1,6 +1,4 @@
 package com.example.fillmeapp.network
-import android.icu.text.CaseMap.Title
-import com.example.fillmeapp.data.Movies
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
--- app/src/main/java/com/example/fillmeapp/network/RetroFitInstance.kt
+++ app/src/main/java/com/example/fillmeapp/network/RetroFitInstance.kt
@@ -4,15 +4,15 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+//import retrofit2.converter.gson.GsonConverterFactory
 
 object RetroFitInstance {
 
     val api by lazy {
         Retrofit.Builder()
             .baseUrl("http://www.omdbapi.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            //.addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            //.addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
             .create(MovieApi::class.java)
     }
